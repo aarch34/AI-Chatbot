51e426c9e1ba9de3bc35632607d80d92b94b3d2a
AmanKanojiya4203 <aman@cuebus.jp>
2024-08-09 23:33:47 +0530

    Fix: Connection to Backend (Check: Postman), State Storage for History format change

--- ai-bot/src/App.js
+++ ai-bot/src/App.js
@@ -1,92 +1,92 @@
-import {useState} from 'react'
+import { useState } from "react";
 
-const App = () =>  {
-  const [value, setValue]=useState("")
-  const [error, setError]=useState("")
-  const [chatHistory,setChatHistory]=useState([])
-  const surpriseOptions=[
+const App = () => {
+  const [value, setValue] = useState("");
+  const [error, setError] = useState("");
+  const [chatHistory, setChatHistory] = useState([]);
+  const surpriseOptions = [
     "Can you tell me a fun fact about space?",
     "Who won the last World Cup?",
     "How do I start learning programming?",
-    "What should I have for dinner tonight?"
-
-  ]
-  const surprise=() =>{
-    const randomValue=surpriseOptions[Math.floor(Math.random()*surpriseOptions.length)]
-    setValue(randomValue)
-
-  }
-  const getResponse=async()=>{
-    if(!value) {
-      setError("Error 0_0 Please ask a question!")
-      return
+    "What should I have for dinner tonight?",
+  ];
+  const surprise = () => {
+    const randomValue =
+      surpriseOptions[Math.floor(Math.random() * surpriseOptions.length)];
+    setValue(randomValue);
+  };
+  const getResponse = async () => {
+    if (!value) {
+      setError("Error 0_0 Please ask a question!");
+      return;
     }
-    try{
-      const options={
-        method:'POST',
-        body:JSON.stringify({
-          history:chatHistory,
-          message:value
+    try {
+      const options = {
+        method: "POST",
+        body: JSON.stringify({
+          history: chatHistory,
+          message: value,
         }),
-        headers:{
-          'Content-Type':'application/json'
-        }
-      }
-      const response=await fetch('http://localhost:3000/gemini',options)
-      const data=await response.text()
-      console.log(data)
-      setChatHistory(oldChatHistory=>[...oldChatHistory,{
-        role:"user",
-        parts:value
-
-      },
-      {
-        role:"model",
-        parts:data
-      }
-    ])
-    setValue("")
-      
-
-    } catch(error){
-      console.error(error)
-      setError("Something went wrong :( Please try again later")
-
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const response = await fetch("http://localhost:8000/gemini", options);
+      console.log(response);
+      const data = await response.text();
+      console.log(data);
+      setChatHistory((oldChatHistory) => [
+        ...oldChatHistory,
+        {
+          role: "user",
+          parts: [{ text: value }],
+        },
+        {
+          role: "model",
+          parts: [{ text: data }],
+        },
+      ]);
+      setValue("");
+    } catch (error) {
+      console.error(error);
+      setError("Something went wrong :( Please try again later");
     }
-
-  }
-  const clear=()=>{
-    setValue("")
-    setError("")
-    setChatHistory([])
-  }
-
+  };
+  const clear = () => {
+    setValue("");
+    setError("");
+    setChatHistory([]);
+  };
 
   return (
-    
-      <div className="app">
-        <p>What do you want to know?
-        <button className="surprise" onClick={surprise} disabled={!chatHistory}>Surprise me! </button>
-        </p>
-        <div className="input-container">
-          <input
-          value={value} placeholder="What day is it today?"
-          onChange={(e)=>setValue(e.target.value)}  />
-          {!error &&<button onClick={getResponse}>Ask me</button>} 
-          {error &&<button onClick={clear}>Clear </button> }
-        </div>
-        {error && <p>{error}</p>}
-        <div className="search-result">
-          {chatHistory.map((chatItem,_index)=><div key={""}>
-            <p className='answer'>{chatItem.role}:{chatItem.parts}</p>
-          </div>)}
-
-        </div>
-
+    <div className="app">
+      <p>
+        What do you want to know?
+        <button className="surprise" onClick={surprise} disabled={!chatHistory}>
+          Surprise me!{" "}
+        </button>
+      </p>
+      <div className="input-container">
+        <input
+          value={value}
+          placeholder="What day is it today?"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        {!error && <button onClick={getResponse}>Ask me</button>}
+        {error && <button onClick={clear}>Clear </button>}
       </div>
-      
-    
-  )
-}
+      {error && <p>{error}</p>}
+      <div className="search-result">
+        {chatHistory.map((chatItem, _index) => (
+          <div key={""}>
+            <p className="answer">
+              {chatItem.role}:{chatItem.parts[0].text}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
